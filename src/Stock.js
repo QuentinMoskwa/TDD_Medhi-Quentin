e3c43828491dd9257bcbf07778eb1a05811cba41
Moskwa Quentin moskwaquentin@gmail.com <95235691+QuentinMoskwa@users.noreply.github.com>
2025-02-07 10:35:17 +0100

    add class article and stock

---
+++ src/Stock.js
@@ -0,0 +1,16 @@
+const Article = require("./Article");
+
+var articles = [
+  new Article(1, "Laptop", 10),
+  new Article(2, "Mouse", 25),
+  new Article(3, "Keyboard", 15),
+  new Article(4, "Monitor", 8),
+  new Article(5, "USB Cable", 30),
+  new Article(6, "Headphones", 12),
+  new Article(7, "Webcam", 9),
+  new Article(8, "Printer", 5),
+  new Article(9, "Router", 14),
+  new Article(10, "External HDD", 7),
+];
+
+// code
