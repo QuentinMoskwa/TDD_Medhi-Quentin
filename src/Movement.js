183f9dac9560a272dd18fefb23211c6c9b9332cf
mahdi173 <zougarimehdi44@gmail.com>
2025-02-21 12:13:19 +0100

    add log in file

--- src/Movement.js
+++ src/Movement.js
@@ -7,6 +7,10 @@ class Movement {
         this.date = date;
         this.quantity = quantity;
     }
+
+    toString() {
+        return `${this.type} | 'Article Id:' ${this.article.id}  |  'Article name:' ${this.article.name} | ${this.date} |  ' Quantity:' ${this.quantity}`;
+    }
 }
 
 module.exports = Movement;
